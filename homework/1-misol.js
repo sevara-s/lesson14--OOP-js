0f078025bcbacc2b7e8012bd6ab84768bb3588b6
Sevara <nematullayevasevara6@gamil.com>
2024-12-13 14:05:20 +0500

    lesson14 OOP js

---
+++ homework/1-misol.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    getInfo() {
+      return `${this.name}'s age is ${this.age}`; 
+    }
+  }
+  
---
+++ homework/2-misol.js
@@ -0,0 +1,10 @@
+class Labrador extends Dog{
+    constructor(name, age, gender, master) {
+      super(name,age,gender,master);        
+      this.species = "Labrador";    
+      this.size = "Large";    
+      this.loyal = true;
+      this.master = master
+    }
+  
+  }
